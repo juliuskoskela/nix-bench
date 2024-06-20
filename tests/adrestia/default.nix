0fea1ce9738dce5da4a769e3e35e81e0eabfcd19
Julius Koskela <julius.koskela@unikie.com>
2024-06-20 14:08:17 +0300

    Added json parser to adrestia example

--- tests/adrestia/default.nix
+++ tests/adrestia/default.nix
@@ -9,7 +9,24 @@
     run = ''
       ${adrestia}/bin/adrestia -a $arrival_rate -l $num_loop -s $service_time -t $nr_threads
     '';
-    parse = "tee";
+    parse = ''
+      input=$(</dev/stdin)
+
+      wakeup_cost_single_us=$(echo "$input" | grep -oP 'wakeup cost \(single\): \K\d+')
+      wakeup_cost_periodic_us=$(echo "$input" | grep -oP 'wakeup cost \(periodic, [0-9]+us\): \K\d+')
+
+      json_output=$(cat <<EOF
+      {
+        "bench_name": "$BENCH_NAME",
+        "job_name": "$JOB_NAME",
+        "wakeup_cost_single_us": $wakeup_cost_single_us,
+        "wakeup_cost_periodic_us": $wakeup_cost_periodic_us
+      }
+      EOF
+      )
+
+      echo "$json_output"
+    '';
   };
 
   logger = "tee";
